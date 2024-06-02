8f38d334534281dbef4a98391d07cf66809451bb
RC22009ues <RC22009@ues.edu.sv>
2024-06-02 11:26:35 -0600

    agregar menu

--- app_Marketing/views.py
+++ app_Marketing/views.py
@@ -9,16 +9,26 @@ def hello(request):
     return HttpResponse("Hello")
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'login.html',{
+        'Curren_page': 'login',
+    })
 
 def mercado(request):
-    return render(request,'Mercado.html')
+    return render(request,'Mercado.html',{
+        'current_page': 'Mercado',
+    })
 
 def planidicar_evento(request):
-    return render(request,'Planificar_evento.html')
+    return render(request,'Planificar_evento.html',{
+        'current_page': 'Planificar_evento',
+    })
 
 def elaborar_campania(request):
-    return render(request,'Elaborar_campania.html')
+    return render(request,'Elaborar_campania.html',{
+        'current_page': 'Elaborar_campania',
+    })
 
 def ver_eventos_campania(request):
-    return render(request,'ver_eventos_campania.html')
+    return render(request,'ver_eventos_campania.html',{
+        'current_page': 'Ver_eventos_campania',
+    })
