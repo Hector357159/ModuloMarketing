12ff5ad3d5846899aa248745d55f09f38db1fffb
RC22009ues <RC22009@ues.edu.sv>
2024-06-02 15:06:56 -0600

    Login

--- app_Marketing/urls.py
+++ app_Marketing/urls.py
@@ -2,14 +2,15 @@ from django.urls import path
 from . import views
 urlpatterns = [
     #agregamos nuestras direcciones con nuestra respuesta o html o htmlresponse
-    path('',views.login),
-    
-    path('Mercado/', views.mercado,name='Mercado'),
+
+    path('', views.mercado,name='Mercado'),
     #
     path('Planificar_evento/', views.planidicar_evento,name='Planificar_evento'),
     
     path('Ver_eventos_campania', views.ver_eventos_campania,name='Ver_eventos_campania'),
     
     path('Elaborar_campania/', views.elaborar_campania,name='Elaborar_campania'),
+    
+    path('logout/', views.exit,name='Exit'),
     
 ]
--- app_Marketing/views.py
+++ app_Marketing/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+from django.shortcuts import render,redirect
+from . import models
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 #responde con http
 from django.http import HttpResponse
 #importamo render para mostrar archivos temporales
@@ -7,17 +11,23 @@ from django.http import HttpResponse
 #creamos las funciones que se mostraran al cliente
 def hello(request):
     return HttpResponse("Hello")
-
+#requere de hacer login
 def login(request):
-    return render(request, 'login.html',{
+    return render(request, 'registration/login.html',{
         'Curren_page': 'login',
     })
 
+
+#reqiere login
+@login_required
 def mercado(request):
     return render(request,'Mercado.html',{
         'current_page': 'Mercado',
     })
 
+#cerrar cesion
+def exit(request):
+    logout(request)
 def planidicar_evento(request):
     return render(request,'Planificar_evento.html',{
         'current_page': 'Planificar_evento',
