7e832faa4685179b51db94188293ffe466f3b7c9
RC22009ues <RC22009@ues.edu.sv>
2024-06-01 21:50:32 -0600

    Agregamos las url para las paginas

--- app_Marketing/views.py
+++ app_Marketing/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+#responde con http
+from django.http import HttpResponse
+#importamo render para mostrar archivos temporales
 # Create your views here.
+
+#creamos las funciones que se mostraran al cliente
+def hello(request):
+    return HttpResponse("Hello")
+
+def login(request):
+    return render(request, 'login.html')
+
+def mercado(request):
+    return render(request,'Mercado.html')
+
+def planidicar_evento(request):
+    return render(request,'Planificar_evento.html')
+
+def elaborar_campania(request):
+    return render(request,'Elaborar_campania.html')
+
+def ver_eventos_campania(request):
+    return render(request,'ver_eventos_campania.html')
