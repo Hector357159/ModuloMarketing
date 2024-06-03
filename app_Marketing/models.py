fed47a3b1114b69e06eab4bb4417e9b1bfe4a4f6
RC22009ues <RC22009@ues.edu.sv>
2024-06-02 18:17:38 -0600

    modelos new

--- app_Marketing/models.py
+++ app_Marketing/models.py
@@ -9,18 +9,6 @@ class RedesSociales(models.Model):
     seguidores = models.PositiveBigIntegerField()
     def __str__(self):
         return self.nombreRedSocia
-
-#modelos de Estadistica
-class Estadistica(models.Model):
-    redSocial = models.OneToOneField(RedesSociales, on_delete=models.CASCADE)
-    seguidores = models.PositiveIntegerField()
-    edad = models.PositiveBigIntegerField()
-    departamento = models.CharField(max_length=100)
-    meGusta = models.PositiveBigIntegerField()
-    
-    def __str__(self) -> str:
-        return self.redSocial
-
 #modelo de  Evento 
 class Campania(models.Model):
     nombreCampania = models.CharField(max_length=100)
@@ -29,10 +17,22 @@ class Campania(models.Model):
     descripcion = models.TextField(blank=True) 
     imagen = models.ImageField(blank=True)
     presupuesto = models.PositiveIntegerField()
-    estadistica = models.ForeignKey(Estadistica, on_delete=models.CASCADE)
     
     def __str__(self) -> str:
         return self.nombreCampania
+
+
+#modelos de Estadistica
+class Estadistica(models.Model):
+    redSocial = models.ForeignKey(RedesSociales, on_delete=models.CASCADE)
+    campania = models.ForeignKey(Campania , on_delete=models.CASCADE)
+    seguidores = models.PositiveIntegerField()
+    edad = models.PositiveBigIntegerField()
+    departamento = models.CharField(max_length=100)
+    meGusta = models.PositiveBigIntegerField()
+    
+    def __str__(self) -> str:
+        return self.redSocial
 
 class Evento(models.Model):
     nombreEvento = models.CharField(max_length=100)
